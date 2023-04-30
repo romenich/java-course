ac5163445ed17f6e0361f38dde721242f460cde0
romenich <romenik.chaves@hotmail.com>
2023-04-30 10:02:00 -0300

    update

--- InicioAulas/src/operadores/Operadores.java
+++ InicioAulas/src/operadores/Operadores.java
@@ -6,10 +6,15 @@ public class Operadores {
 		System.out.println("Operadores em java: ");
 		System.out.println("Operador de atribuição: (=)" );
 		System.out.println("Operadores aritméticos: adição (+), subtração (-), multiplicação (*), e divisão (/)");
-		System.out.println();
-		int numero = 5;
+		System.out.println("Operadores relacionais: == verifica se o valor de variáveis são iguais\n"
+				+ "!= verifica se o valor de variáveis são diferentes\n"
+				+ "> verifica se uma variável é maior que a outra\n"
+				+ ">= verifica se uma variável é maior ou igual  outra\n"
+				+ "< verifica se uma variável é menor que a outra\n"
+				+ "<= verifica se uma variável é menor ou igual a outra");
+		/*int numero = 5;
 		do {numero = numero + 5;
-		System.out.println(numero);} while (numero<100);
+		System.out.println(numero);} while (numero<100);*/
 		
 		
 		
