de709aee1cd64ddd00bc313e2dff20ab9d13fe71
romenich <romenik.chaves@hotmail.com>
2023-04-30 09:31:30 -0300

    update

---
+++ InicioAulas/src/operadores/Operadores.java
@@ -0,0 +1,17 @@
+package operadores;
+
+public class Operadores {
+
+	public static void main(String[] args) {
+		System.out.println("Operadores em java: ");
+		System.out.println("Operador de atribuição: (=)" );
+		System.out.println("Operadores aritméticos: adição (+), subtração (-), multiplicação (*), e divisão (/)");
+		System.out.println();
+		int numero = 5;
+		do {numero = numero + 5;
+		System.out.println(numero);} while (numero<100);
+		
+
+	}
+
+}
